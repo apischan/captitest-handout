5d1d73c71e107df9c5775acdbe7b26446eaacc71
apischan <andrew.pischanski@gmail.com>
2016-06-23 02:16:45 +0300

    added test for approximatesFor() function

--- src/test/scala/captify/test/scala/TestAssignmentSpec.scala
+++ src/test/scala/captify/test/scala/TestAssignmentSpec.scala
@@ -4,6 +4,8 @@ import captify.test.scala.SparseIterators.iteratorFromOne
 import captify.test.scala.TestAssignment._
 import org.scalatest.{FlatSpec, Matchers}
 
+import scala.util.Try
+
 class TestAssignmentSpec extends FlatSpec with Matchers {
 
   "iteratorFromOne" should "return iterator with subsequence" in {
@@ -26,6 +28,17 @@ class TestAssignmentSpec extends FlatSpec with Matchers {
     val result = mergeIterators(iterators)
     val expected = Seq[BigInt](1, 1, 2, 2, 3, 3).toIterator
     iteratorsValuesEquals(6)(result, expected)
+  }
+
+  // ugly test but I was not able to invent better
+  "approximatesFor" should "return approximates for given sparsity" in {
+    val extent = 1000
+    val (sparseMin, sparseMax) = (2, 4)
+    val expected = (sparseMin to sparseMax) map {
+      sparsity => (sparsity, Try(approximateSparsity(sparsity, extent)))
+    }
+    val actual = approximatesFor(2, 4, extent)
+    actual should be (expected)
   }
 
   def iteratorsValuesEquals(amount: Int)(result: Iterator[BigInt], expected: Iterator[BigInt]) =
